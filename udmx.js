e0ad71664f19538ae8339de2f62c3352e6227646
John Roper <johnroper100@gmail.com>
2018-08-25 12:58:49 -0400

    Fix for timeout issues

--- udmx.js
+++ udmx.js
@@ -1,47 +1,50 @@
 var usb = require('usb');
 var events = require('events');
 
-class DMX extends events.EventEmitter {
-
-  constructor(options) {
-    var defaults = {vendor: 0x16c0, device: 0x5dc};
+function DMX(options) {
+    var defaults = {
+        vendor: 0x16c0,
+        device: 0x5dc
+    };
 
     for(var opt in defaults) {
-      if(!defaults.hasOwnProperty(opt)) continue;
-      this[opt] = options && options.hasOwnProperty(opt) ? options[opt] : defaults[opt];
+        if(!defaults.hasOwnProperty(opt)) continue;
+        this[opt] = options && options.hasOwnProperty(opt) ? options[opt] : defaults[opt];
     }
 
     this.connected = false;
-    this.state = {};
-  }
+    this.state = {}
+}
 
-  connect() {
+DMX.prototype.__proto__ = events.EventEmitter.prototype;
+
+DMX.prototype.connect = function() {
     this.dev = usb.findByIds(this.vendor, this.device);
     if(this.dev === undefined) {
-     throw 'Unable to find USB device!';
+       throw 'Unable to find USB device!';
     }
 
    this.dev.open();
    this.emit('connected');
-  }
-
-  set(channel, value) {
-      var self = this;
-      return new Promise((resolve, reject) => {
-          this.dev.controlTransfer(64, 1, value, channel-1, Buffer(1), function(err, result) {
-              if(err) {
-                  reject(err);
-              } else {
-                  self.emit('channel-' + channel, value);
-                  self.emit('channel-all', channel, value);
-                  self.state[channel] = value;
-                  resolve(result);
-              }
-          });
-      });
-  }
-
-  get(channel) {
-      return this.state[channel];
-  }
 }
+
+DMX.prototype.set = function(channel, value) {
+    var self = this;
+    return new Promise((resolve, reject) => {
+        this.dev.controlTransfer(0x40, 0x0002, value, channel-1, Buffer.alloc(1), 0, function(err, result) {
+            if(err) {
+                console.log(err);
+            } else {
+                self.emit('channel-' + channel, value);
+                self.emit('channel-all', channel, value);
+                self.state[channel] = value;
+            }
+        });
+    });
+}
+
+DMX.prototype.get = function(channel) {
+    return this.state[channel];
+}
+
+module.exports = DMX;
